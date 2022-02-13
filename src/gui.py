1d5928363c662cf5feb536e4e00a523b682fc6d9
Andrew Li <andrew@Andrews-MacBook-Pro.local>
2022-02-13 00:38:51 -0500

    gui stuff

--- src/gui.py
+++ src/gui.py
@@ -1,30 +1,40 @@
 import tkinter as tk
+from functools import partial
 
-ingredients = ["milk", "rabbit", "juice", "calamari", "hot dog", "orange", "penis"]
+ingredients = ["milk", "rabbit", "juice", "calamari", "hot dog", "orange", "penis", "frog", "ginger", "arsenic"]
 window = None
 sv = None
 
+# on ingredient click
+def ing_click(ing):
+    print(ing)
+
 # load all ingredient buttons
-def loadIngredientButtons():
+def load_ingredient_buttons():
+    frame1 = tk.Frame(window)
+    frame1.pack(pady=20)
     for ing in ingredients:
+        b = tk.Button(frame1, text=ing, command=partial(ing_click, ing))
+        b.pack(side="left")
         pass
+
 # on text box change
-def textBoxChanged(a, b, c):
+def text_box_changed(a, b, c):
     # update ingredients list
-    loadIngredientButtons()
+    load_ingredient_buttons()
 
-def loadIngredientScreen():
-    sv.trace_add("write", textBoxChanged)
-    searchBar = tk.Entry(window, width=20, bg="white", fg="black", textvariable=sv)
-    searchBar.pack()
-
+def load_ingredient_screen():
+    sv.trace_add("write", text_box_changed)
+    search_bar = tk.Entry(window, width=20, bg="white", fg="black", textvariable=sv)
+    search_bar.pack(pady=20)
+    load_ingredient_buttons()
 
 if __name__ == "__main__":
     window = tk.Tk()
     sv = tk.StringVar()
     window.title("Recipe Recommender 1.0")
     window.geometry("500x800")
-    loadIngredientScreen()
+    load_ingredient_screen()
     window.mainloop()
 
 
