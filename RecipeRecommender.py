6c4f909d08f0beed03e59338145beda115e60143
Xinyu Wu <xinyuwu421@gmail.com>
2022-02-13 05:52:13 -0500

    change notebook to .py

--- RecipeRecommender.py
+++ RecipeRecommender.py
@@ -1,63 +1,30 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[67]:
-
-
 import requests
 import json
 import pandas as pd
 import re
 
-
-# In[68]:
-
-
+# read in data, store as dataframe
 df = pd.read_csv('dataframe.csv')
 df.dropna(inplace=True)
 df.drop(['Unnamed: 0','Image_Name','Ingredients'], axis=1, inplace=True)
 df['Cleaned_Ingredients'] = df['Cleaned_Ingredients'].str.replace("\d+", '')
 df['Cleaned_Ingredients'] = df['Cleaned_Ingredients'].str.replace("/", '')
-df
-
-
-# In[69]:
-
-
 text_file = open("ingredients.txt", "r")
  
 #read whole file to a string
 ingre = text_file.read()
-
 text_file.close()
-
 ingredient_list = ingre.splitlines()
-print(ingredient_list)
 
+# add ingredients into dictionary with value being empty list
 ingredient_dict = {}
 for string in ingredient_list:
     if string not in ingredient_dict:
         ingredient_dict[string] = []
 
-print(ingredient_dict)
-
-
-# In[70]:
-
-
+# add recipes with corresponding ingredients to empty list
 for ingredient in ingredient_list:
     ingredient_dict[ingredient] = df.loc[df['Cleaned_Ingredients'].str.contains(ingredient, case=False)]['Title'].values.tolist()
-  
-
-
-# In[71]:
-
-
-ingredient_dict
-
-
-# In[64]:
-
 
 # append ingredient names to user_input_list
 user_input_list = []
@@ -72,34 +39,9 @@ for i in range(1, len(output)):
     prev_lst = set(output[i-1])
     common_elements = prev_lst.intersection(output[i])
 
-print(common_elements)
-
-
-# In[65]:
-
-
 # initialize dictionary of returned recipes as keys
 recipes = dict.fromkeys(common_elements, 0)
-print(recipes)
-
-
-# In[66]:
-
-
 # maps instructions as value to each recipe in dictionary
 for r in recipes.keys():
     recipes[r] = df.loc[df['Title'].str.contains(r, case=False)]['Instructions'].values.tolist()
 print(recipes)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
